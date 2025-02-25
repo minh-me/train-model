375a54255b9eead6885319b5c44d0fbed719f8b6
minh <minhchiu.official@gmail.com>
2025-02-25 08:51:04 +0700

    feat: add fastapi

--- predict.py
+++ predict.py
@@ -5,7 +5,7 @@ import sys
 model = YOLO("runs/classify/train/weights/best.pt")
 
 # Ảnh cần phân loại (truyền qua dòng lệnh hoặc đặt sẵn)
-image_path = sys.argv[1] if len(sys.argv) > 1 else "D:/yolo3/image-test/diamant.jpg"
+image_path = sys.argv[1] if len(sys.argv) > 1 else "D:/yolo3/image-test/rubin.jpg"
 
 # Dự đoán
 results = model(image_path)
