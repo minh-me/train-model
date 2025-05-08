c256788de5d593857546660a338405d21f61e461
minh chiu <himinhchiu@gmail.com>
2025-05-08 02:40:51 +0000

    feat: update coins euro

--- predict.py
+++ predict.py
@@ -2,10 +2,10 @@ from ultralytics import YOLO
 import sys
 
 # Load mô hình đã huấn luyện
-model = YOLO("D:/yolo3/runs/classify/train10/weights/best.pt")
+model = YOLO("/home/ubuntu/train-model/runs/classify/euro-coins/weights/best.pt")
 
 # Ảnh cần phân loại (truyền qua dòng lệnh hoặc đặt sẵn)
-image_path = sys.argv[1] if len(sys.argv) > 1 else "D:/yolo3/leaf/guava/tomato.png"
+image_path = sys.argv[1] if len(sys.argv) > 1 else "/home/ubuntu/train-model/EuroCoins-1/test/2018/comm_2018_latvia_joint_jpg.rf.40af94668d10e043af6c2428a51a8714.jpg"
 
 # Dự đoán
 results = model(image_path)
--- train_model.py
+++ train_model.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO("yolo11n-cls.pt")  
 
 # Huấn luyện với tập dữ liệu đã tải về
-model.train(data="D:/yolo3/EuroCoins-1", epochs=50, imgsz=224)
+model.train(data="/home/ubuntu/train-model/EuroCoins-1", epochs=100, imgsz=224)
